4c61ed71bc69f1c61020535ac890005db7cf5fc6
bajpai244 <bajpaiharsh244@gmail.com>
2022-06-06 17:44:37 +0530

    refactor(register): refactory register button

---
+++ components/register.tsx
@@ -0,0 +1,38 @@
+/** @format */
+
+import { FunctionComponent } from 'react';
+import { Box, Link, Text } from 'theme-ui';
+
+const Register: FunctionComponent<{ link: string }> = ({ link }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        my: [1, 1, 0],
+      }}
+    >
+      <Link href={link} target="_blank">
+        <Text
+          sx={{
+            textShadow: 'none',
+            boxShadow: '3px 3px 7px #42ff6b',
+            borderRadius: '4px',
+            p: [2, 2, 2],
+            fontSize: ['1rem', '1.5rem', '1.5rem'],
+            borderColor: 'text',
+            borderStyle: 'solid',
+            borderWidth: '5px',
+            height: 'inherit',
+          }}
+        >
+          Register Now
+        </Text>
+      </Link>
+    </Box>
+  );
+};
+
+export default Register;
