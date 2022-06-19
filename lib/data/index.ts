ccb829e93012fda33b9dfafb8ead5aaa769911d2
bajpai244 <bajpaiharsh244@gmail.com>
2022-06-19 13:14:26 +0530

    chore(data): add data for meta and registration calendar link

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -1,1 +1,7 @@
 export const registerLink = 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20220625T133000Z%2F20220625T143000Z&details=Hey%20there%21%0A%0AWe%20will%20be%20doing%20a%20ZK-SNARK%20workshop%21%20ZK-SNARKs%20are%20a%20way%20to%20prove%20the%20existence%20of%20a%20piece%20of%20information%20without%20actually%20revealing%20it.%0A%0AThis%20will%20be%20for%20Hack%20Club%20members%21%20Feel%20free%20to%20join%2C%20let%27s%20have%20fun%2C%20and%20let%27s%20keep%20the%20hacker%20spirit%20alive.&location=Zoom%20%7B%20link%20will%20be%20shared%20in%20slack%20thread%20%7D&text=ZK-Snark%20Workshop'
+
+export const meta = {
+	title: "ZK-Elixir",
+	description: "Learn ZK-SNARK in 60 minutes, for complete beginners.",
+	imgLink: "https://cloud-p84fck8ij-hack-club-bot.vercel.app/0image.png"
+}
